7367e40ba138dec1d5ecab19421ca9ab49e113ac
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-06-16 00:41:39 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,11 @@ buildscript {
         classpath(libs.kotlin.gradle.plugin)
         classpath(libs.navigation.safe.args.gradle.plugin)
     }
-    extra["apple"] = mapOf("emoji" to "🍎", "price" to 123)
+    extra["apple"] = mapOf("emoji" to "🍎", "price" to 42)
 }
 
 plugins {
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.compose.compiler) apply false // https://developer.android.com/develop/ui/compose/compiler#version-catalog
+    alias(libs.plugins.hilt) apply false // https://developer.android.com/training/dependency-injection/hilt-android#setup
+    alias(libs.plugins.ksp) apply false // https://developer.android.com/build/migrate-to-ksp#add-ksp
 }
